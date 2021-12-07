1c9b2b448ed176c882c142ac8ce6a7302cfe005b
Martin Polden <mpolden@yahoo-inc.com>
2021-12-07 14:21:31 +0100

    aoc21: Fix function name

--- aoc21/day07.py
+++ aoc21/day07.py
@@ -7,11 +7,11 @@ from util import file_input, text_input, ints
 example_input = "16,1,2,0,4,2,7,1,2,14"
 
 
-def optimal_position(initial: List[int], fixed_burn_rate: bool = True) -> int:
+def min_consumption(positions: List[int], fixed_burn_rate: bool = True) -> int:
     fuel_required: Dict[int, int] = defaultdict(int)
-    positions = range(0, max(initial) + 1)
-    for from_pos in initial:
-        for to_pos in positions:
+    possible_positions = range(0, max(positions) + 1)
+    for from_pos in positions:
+        for to_pos in possible_positions:
             fuel_spent = abs(to_pos - from_pos)
             if not fixed_burn_rate:
                 fuel_spent = int((fuel_spent ** 2 + fuel_spent) / 2)
@@ -20,7 +20,7 @@ def optimal_position(initial: List[int], fixed_burn_rate: bool = True) -> int:
 
 
 def day7_1(nums: List[int]) -> int:
-    return optimal_position(nums)
+    return min_consumption(nums)
 
 
 assert day7_1(text_input(example_input, ints)[0]) == 37
@@ -28,7 +28,7 @@ assert day7_1(file_input(7, ints)[0]) == 345035
 
 
 def day7_2(nums: List[int]) -> int:
-    return optimal_position(nums, fixed_burn_rate=False)
+    return min_consumption(nums, fixed_burn_rate=False)
 
 
 assert day7_2(text_input(example_input, ints)[0]) == 168
