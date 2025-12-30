ab37dfc9ce0bcb35a20afe011c535fde9020c7ae
Martin Polden <mpolden@mpolden.no>
2025-12-30 21:33:07 +0100

    aoc25: day 6b

--- aoc25/day06_test.go
+++ aoc25/day06_test.go
@@ -1,6 +1,8 @@
 package aoc25
 
 import (
+	"bufio"
+	"bytes"
 	"io"
 	"strings"
 	"testing"
@@ -40,13 +42,67 @@ func homework(r io.Reader) int {
 	return total
 }
 
+func homework2(r io.Reader) int {
+	var lines [][]rune
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, runes(scanner.Text()))
+	}
+	var (
+		ops  []rune
+		cols []int
+	)
+	for i, op := range lines[len(lines)-1] {
+		if op == ' ' {
+			continue
+		}
+		cols = append(cols, i)
+		ops = append(ops, op)
+	}
+	var (
+		buf   bytes.Buffer
+		total int
+	)
+	for i, start := range cols {
+		end := len(lines[0])
+		if i < len(cols)-1 {
+			end = cols[i+1] - 1
+		}
+		sum := 0
+		for j := start; j < end; j++ {
+			buf.Reset()
+			for _, line := range lines[:len(lines)-1] {
+				d := line[j]
+				if d == ' ' {
+					continue
+				}
+				buf.WriteRune(d)
+			}
+			n := atoi(buf.String())
+			switch ops[i] {
+			case '+':
+				sum += n
+			case '*':
+				if sum == 0 {
+					sum = 1
+				}
+				sum *= n
+			}
+		}
+		total += sum
+	}
+	return total
+}
+
 func TestDay06(t *testing.T) {
 	example := `
-123 328  51 64
- 45 64  387 23
+123 328  51 64 
+ 45 64  387 23 
   6 98  215 314
-*   +   *   +
+*   +   *   +  
 `
 	check(t, 4277556, homework, readString(example))
 	check(t, 4951502530386, homework, readInput(6))
+	check(t, 3263827, homework2, readString(example))
+	check(t, 8486156119946, homework2, readInput(6))
 }
